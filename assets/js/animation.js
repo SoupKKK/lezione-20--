1973cacaab2e34491576f7d8e9809059adb49fd9
“giuseppe <giuseppeforce36@email.com>
2023-11-24 16:50:55 +0100

    animazione piu fluida

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -13,20 +13,22 @@ window.addEventListener('scroll', function() {
 //animazione M letters
 const ms = document.querySelectorAll('svg text');
 
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
-  function toggleMs() {
-    ms.forEach((m) => {
-      const randomNum = getRandomInt(10);
+function toggleMs() {
+  ms.forEach((m) => {
+    const randomNum = getRandomInt(10);
 
-      if (randomNum >= 5) {
-        m.setAttribute('opacity', '0');
-      } else {
-        m.setAttribute('opacity', '1');
-      }
-    });
-  }
+    m.style.transition = 'opacity 0.5s ease-in-out';
 
-  setInterval(toggleMs, 1000);
+    if (randomNum >= 5) {
+      m.style.opacity = '0';
+    } else {
+      m.style.opacity = '1';
+    }
+  });
+}
+
+setInterval(toggleMs, 2000);
